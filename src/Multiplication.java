2ed1503ce598942dbb3399c3292906a159f26bbc
perfect-Bee <joonslee3@gmail.com>
2026-01-21 09:19:17 +0900

    코드카타 알고리즘 2. 두 수의 곱 계산

---
+++ src/Multiplication.java
@@ -0,0 +1,10 @@
+public class Multiplication {
+    public int multiplication(int num1, int num2) {
+       int answer = 0;
+       if (0 <= num1 && num2 <= 100) {
+           answer = num1 * num2;
+       }
+       return answer;
+    }
+}
+
