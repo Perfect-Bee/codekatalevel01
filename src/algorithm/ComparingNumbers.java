379db7293119b3cd34d06317369c7dc4549324eb
perfect-Bee <joonslee3@gmail.com>
2026-01-29 09:25:22 +0900

    코드카타 알고리즘 5. 숫자 비교하기

---
+++ src/algorithm/ComparingNumbers.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+class ComparingNumbers {
+    public int comparingnumbers(int num1, int num2) {
+        int answer = 0;
+        if (0 <= num1 && num2 <= 10000) {
+            if (num1 == num2) {
+                answer = 1;
+            } answer = -1;
+        }
+        return answer;
+    }
+}
