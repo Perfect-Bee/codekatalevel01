a5bc5782ea8d84f77b50e8a9aee154838d380039
perfect-Bee <joonslee3@gmail.com>
2026-02-05 09:51:48 +0900

    코드카타 알고리즘 7. 각도기

---
+++ src/algorithm/Angle.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+public class Angle {
+    public int angle (int angle) {
+        int answer = 0;
+        // 범위가 0 ~ 180
+        if (angle < 90) {           //예각
+            answer = 1;
+        } else if (angle == 90) {   // 직각
+            answer = 2;
+        } else if (angle < 180) {   // 둔각
+            answer = 3;
+        } else {                    // 평각
+            answer = 4;
+        }
+        return answer;
+    }
+}
